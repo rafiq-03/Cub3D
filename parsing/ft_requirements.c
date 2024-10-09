3a1daefbc2900c6c9aee16a673a1d5044bc7c87c
Rafiq Marzouk <rafiq.marzouk@gmail.com>
2024-10-09 16:28:52 +0100

    handle textures

--- parsing/ft_requirements.c
+++ parsing/ft_requirements.c
@@ -6,33 +6,82 @@
 /*   By: rmarzouk <rmarzouk@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 15:06:10 by rmarzouk          #+#    #+#             */
-/*   Updated: 2024/10/05 16:26:55 by rmarzouk         ###   ########.fr       */
+/*   Updated: 2024/10/09 16:19:41 by rmarzouk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3D.h"
 
-int check_requirements(int ac, char **av)
-{
-	int	fd;
+void	check_args(int ac, char *str);
+void fill_map(int fd, t_data *data);
 
+void	check_args(int ac, char *str)
+{
 	if (ac != 2)
 	{
-		printf("invalid arguments\n");
-		return (1);
+		ft_strerr("Prototype : ./cub3D [ path_to_map.cub ]");
+		exit (1);
 	}
-	if (ft_strncmp(".cub", av[1] + ft_strlen(av[1]) - 4, 4))
+	if (ft_strcmp(".cub", str + ft_strlen(str) - 4))
 	{
-		printf("extension must be \' ___.cub \'\n");
-		return (1);
+		ft_strerr("extension must be [ ___.cub ]");
+		exit (1);
 	}
+}
+
+void	fill_file_content(t_data *data, int fd)
+{
+	char *line;
+	int		i;
+	t_list	*tmp;
+
+	i = 0;
+	line = get_next_line(fd);
+	data->file_content = ft_lstnew(line);
+	data->file_content->i = i;
+	line = get_next_line(fd);
+	while (line)
+	{
+		tmp = ft_lstnew(line);
+		ft_lstadd_back(&data->file_content, tmp);
+		line = get_next_line(fd);
+	}
+	// free this fucking data we don't want leaks
+	close(fd);
+}
+
+void	check_requirements(int ac, char **av, t_data *data)
+{
+	int fd;
+
+	check_args(ac, av[1]);
 	fd = open (av[1], O_RDONLY);
 	if (fd < 0)
 	{
-		printf("failed to open the file\n");
-		return (1);
+		perror("error\n => ");
+		exit (EXIT_FAILURE);
 	}
-	
-	return (0);
-	
+	fill_file_content(data, fd);
+	fill_textures(data);
+	fill_colors(data);
+	// printf("this is me\n");
+	// fill_map(fd, data);
+
+}
+
+void fill_map(int fd, t_data *data)
+{
+	char	*line;
+	char	*tmp_map;
+	int		i;
+
+	i = 0;
+	tmp_map = ft_strdup("");
+	line = get_next_line(fd);
+	while (line)
+	{
+		tmp_map = ft_strjoin(tmp_map, line);
+		line = get_next_line(fd);
+	}
+	data->map=ft_split(tmp_map, '\n');	
 }
---
+++ parsing/ft_textures.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_textures.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarzouk <rmarzouk@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/06 13:36:59 by rmarzouk          #+#    #+#             */
+/*   Updated: 2024/10/09 16:24:38 by rmarzouk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../cub3D.h"
+
+void	add_texture(char **texture, char *line, u_int16_t *flag);
+void	clean_textures(t_texture *textures);
+void	fill_textures(t_data *data);
+
+
+void	add_texture(char **texture, char *line, u_int16_t *flag)
+{
+
+	if (*texture)
+	{
+		ft_strerr("duplicate texture");
+		exit (1);
+	}
+	*texture = ft_strtrim(line, "\n");
+	(*flag)++;
+}
+void	clean_textures(t_texture *textures)
+{
+	textures->EA = NULL;
+	textures->WE = NULL;
+	textures->SO = NULL;
+	textures->NO = NULL;
+	textures->flag = 0;
+}
+
+void	fill_textures(t_data *data)
+{
+	char **split;
+	t_list *tmp;
+	int		words;
+
+	words = 0;
+	tmp = data->file_content;
+	clean_textures(&data->textures);
+	while (tmp && data->textures.flag < 4)
+	{
+		split = ft_split_ws(tmp->content, " \n", &words);//split content of line by spaces and new line to trim it
+		if (words == 2)
+		{
+			if (!ft_strcmp(split[0], "SO"))
+				add_texture(&data->textures.SO, split[1], &data->textures.flag);
+			if (!ft_strcmp(split[0], "NO"))
+				add_texture(&data->textures.NO, split[1], &data->textures.flag);
+			if (!ft_strcmp(split[0], "WE"))
+				add_texture(&data->textures.WE, split[1], &data->textures.flag);
+			if (!ft_strcmp(split[0], "EA"))
+				add_texture(&data->textures.EA, split[1], &data->textures.flag);
+		}
+		free(split);
+		tmp = tmp->next;
+	}
+	if (data->textures.flag != 4)
+	{
+		ft_strerr("A texture is missing");
+		exit(EXIT_FAILURE);	
+	}
+	// printf("SO %s\n", data->textures.SO);
+	// printf("NO %s\n", data->textures.NO);
+	// printf("EA %s\n", data->textures.EA);
+	// printf("WE %s\n", data->textures.WE);
+}
